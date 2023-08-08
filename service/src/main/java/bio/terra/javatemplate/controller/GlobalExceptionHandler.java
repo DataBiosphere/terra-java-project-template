5b76284fc25cabe56464a1ce3b89aaed9185c5a1
Phil Shapiro <pshapiro@broadinstitute.org>
2023-08-08 15:16:59 -0400

    bubble up an error report exception if it occurs inside a bean creation context

--- service/src/main/java/bio/terra/javatemplate/controller/GlobalExceptionHandler.java
+++ service/src/main/java/bio/terra/javatemplate/controller/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package bio.terra.javatemplate.controller;
 
 import bio.terra.common.exception.AbstractGlobalExceptionHandler;
+import bio.terra.common.exception.ErrorReportException;
 import bio.terra.javatemplate.model.ErrorReport;
 import java.util.List;
+import org.springframework.beans.BeanInstantiationException;
+import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,6 +14,17 @@ public class GlobalExceptionHandler extends AbstractGlobalExceptionHandler<Error
 
   @Override
   public ErrorReport generateErrorReport(Throwable ex, HttpStatus statusCode, List<String> causes) {
+    // If a bean creation failed due to an error report exception, use that exception
+    // to generate the error report. This lets us provide the correct status code and a more useful message.
+    //
+    // This is specific for BeanCreationException, but it might make sense to always look for an ErrorReportException
+    // in the cause chain and use it if found.
+    if (ex instanceof BeanCreationException
+        && ex.getCause() instanceof BeanInstantiationException
+        && ex.getCause().getCause() instanceof ErrorReportException ere) {
+      ex = ere;
+      statusCode = ere.getStatusCode();
+    }
     return new ErrorReport().message(ex.getMessage()).statusCode(statusCode.value());
   }
 }
