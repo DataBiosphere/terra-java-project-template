f90133e14d74c1c619fe7ca970a68f6eb6d13a16
Chip Pate <pate@broadinstitute.org>
2024-02-29 23:13:15 -0500

    chore: remove dead file

--- scripts/init-db/validate_psql.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# validate postgres startup
-
-echo "sleeping for 5 seconds during postgres boot..."
-sleep 5
-PGPASSWORD="${DATABASE_USER_PASSWORD:-dbpwd}" \
-  psql --username "${DATABASE_USER:-dbuser}" -d "${DATABASE_NAME:-javatemplate_db}" \
-       -c "SELECT VERSION(); SELECT NOW()"
