c7ee35e7718ec108097bc94d6a27e150e878ad3d
Chip Pate <pate@broadinstitute.org>
2024-02-29 22:42:22 -0500

    feat: simplifying database setup and introduce service

--- scripts/init-db/postgres-init.sql
+++ scripts/init-db/postgres-init.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE javatemplate_db;
 CREATE ROLE dbuser WITH LOGIN ENCRYPTED PASSWORD 'dbpwd';
-GRANT CREATE ON DATABASE javatemplate_db to dbuser;
+GRANT CREATE ON DATABASE javatemplate_db TO dbuser;
+-- GRANT ALL PRIVILEGES ON DATABASE javatemplate_db TO dbuser;
