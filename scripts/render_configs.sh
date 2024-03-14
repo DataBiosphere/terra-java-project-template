da0dc47981d2487e89b5f52eec17fb2471390d85
Chip Pate <pate@broadinstitute.org>
2024-03-14 18:18:31 -0400

    build: integration tests work

--- scripts/render_configs.sh
+++ scripts/render_configs.sh
@@ -14,8 +14,8 @@ if ! [ -x "$(command -v vault)" ]; then
   VAULT_COMMAND="docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN -e VAULT_ADDR=$VAULT_ADDR vault:1.7.3 $VAULT_COMMAND"
 fi
 
-$VAULT_COMMAND -field=data -format=json "secret/dsde/firecloud/$ENV/common/firecloud-account.json" >"$INTEGRATION_OUTPUT_LOCATION/user-delegated-sa.json"
+$VAULT_COMMAND -address=$VAULT_ADDR -field=data -format=json "secret/dsde/firecloud/$ENV/common/firecloud-account.json" >"$INTEGRATION_OUTPUT_LOCATION/user-delegated-sa.json"
 
 # we use the perf testrunner account in all environments
 PERF_VAULT_PATH="secret/dsde/terra/kernel/perf/common"
-$VAULT_COMMAND -field=key "$PERF_VAULT_PATH/testrunner/testrunner-sa" | base64 -d > "$INTEGRATION_OUTPUT_LOCATION/testrunner-perf.json"
+$VAULT_COMMAND -address=$VAULT_ADDR -field=key "$PERF_VAULT_PATH/testrunner/testrunner-sa" | base64 -d > "$INTEGRATION_OUTPUT_LOCATION/testrunner-perf.json"
